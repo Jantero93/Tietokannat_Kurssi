49aa9c8613f7d4488703e9da87e536980ad6e130
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2020-12-06 21:41:58 +0200

    lisätty delete artist/museum/artpiece

--- backend/src/index.js
+++ backend/src/index.js
@@ -544,6 +544,93 @@ todos.del(todoPath, async (ctx) => {
   }
 });
 
+// DELETE /resource/:id (taideteokset)
+todos.del(artpiecePath, async (ctx) => {
+  const { id } = ctx.params;
+  console.log('.del id contains:', id);
+
+  if (isNaN(id) || id.includes('.')) {
+    ctx.throw(400, 'id must be an integer');
+  }
+
+  try {
+    const conn = await mysql.createConnection(connectionSettings);
+    const [status] = await conn.execute(`
+          DELETE FROM taideteokset
+          WHERE taideteos_id = :id;
+        `, { id });
+
+    if (status.affectedRows === 0) {
+      // The row did not exist, return '404 Not found'
+      ctx.status = 404;
+    } else {
+      // Return '204 No Content' status code for successful delete
+      ctx.status = 204;
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+    ctx.throw(500, error);
+  }
+});
+
+// DELETE /resource/:id (taiteilijat)
+todos.del(artistPath, async (ctx) => {
+  const { id } = ctx.params;
+  console.log('.del id contains:', id);
+
+  if (isNaN(id) || id.includes('.')) {
+    ctx.throw(400, 'id must be an integer');
+  }
+
+  try {
+    const conn = await mysql.createConnection(connectionSettings);
+    const [status] = await conn.execute(`
+          DELETE FROM taiteilijat
+          WHERE taiteilija_id = :id;
+        `, { id });
+
+    if (status.affectedRows === 0) {
+      // The row did not exist, return '404 Not found'
+      ctx.status = 404;
+    } else {
+      // Return '204 No Content' status code for successful delete
+      ctx.status = 204;
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+    ctx.throw(500, error);
+  }
+});
+
+// DELETE /resource/:id (museot)
+todos.del(museumPath, async (ctx) => {
+  const { id } = ctx.params;
+  console.log('.del id contains:', id);
+
+  if (isNaN(id) || id.includes('.')) {
+    ctx.throw(400, 'id must be an integer');
+  }
+
+  try {
+    const conn = await mysql.createConnection(connectionSettings);
+    const [status] = await conn.execute(`
+          DELETE FROM museot
+          WHERE museo_id = :id;
+        `, { id });
+
+    if (status.affectedRows === 0) {
+      // The row did not exist, return '404 Not found'
+      ctx.status = 404;
+    } else {
+      // Return '204 No Content' status code for successful delete
+      ctx.status = 204;
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+    ctx.throw(500, error);
+  }
+});
+
 app.use(test.routes());
 app.use(test.allowedMethods());
 app.use(todos.routes());
