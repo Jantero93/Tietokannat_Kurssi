c1515e970ca91822d6aba69644bd8ddd23ab3ad4
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2020-12-07 12:43:27 +0200

    dockerin alustukseen lisätty datan vienti tauluihin

--- backend/src/fixtures/initDB.js
+++ backend/src/fixtures/initDB.js
@@ -44,4 +44,90 @@ export default async () => {
       console.log('...success!');
     }
   }
+  try {
+    await conn.execute(`
+        SELECT *
+        FROM museot
+      `);
+  } catch (error) {
+    // If table does not exist, create it
+    if (error.errno === 1146) {
+      console.log('Initializing table: museot');
+      await conn.execute(`
+      CREATE TABLE museot (
+        museo_id int UNSIGNED AUTO_INCREMENT,
+        museoNimi varchar(100) NOT NULL,
+        maa varchar(100),
+        kaupunki varchar(100),
+        PRIMARY KEY (museo_id)
+        )
+      `);
+      await conn.execute(`
+      INSERT INTO museot (museo_id, museoNimi, maa, kaupunki)
+        VALUES ("1", "Yksityinen", "", ""),
+        ("2", "Ateneum", "Suomi", "Helsinki"),
+        ("3", "Louvre", "Ranska", "Pariisi")
+      `);
+      console.log('...success!');
+    }
+  }
+  try {
+    await conn.execute(`
+        SELECT *
+        FROM taiteilijat
+      `);
+  } catch (error) {
+    // If table does not exist, create it
+    if (error.errno === 1146) {
+      console.log('Initializing table: taiteilijat');
+      await conn.execute(`
+      CREATE TABLE taiteilijat (
+        taiteilija_id int UNSIGNED AUTO_INCREMENT,
+        tekija varchar(100) NOT NULL,
+        kansalaisuus varchar(100),
+        PRIMARY KEY (taiteilija_id)
+        )
+      `);
+      await conn.execute(`
+      INSERT INTO taiteilijat (taiteilija_id, tekija, kansalaisuus)
+        VALUES ("1", "Paul Gauguin", "Ranska"),
+        ("2", "Ferdinand von Wright", "Suomi"),
+        ("3", "Gustav Klimt", "Itävalta")
+      `);
+      console.log('...success!');
+    }
+  }
+  try {
+    await conn.execute(`
+        SELECT *
+        FROM taideteokset
+      `);
+  } catch (error) {
+    // If table does not exist, create it
+    if (error.errno === 1146) {
+      console.log('Initializing table: taideteokset');
+      await conn.execute(`
+      CREATE TABLE taideteokset (
+        taideteos_id int UNSIGNED AUTO_INCREMENT,
+        tekovuosi int,
+        aihe varchar(100),
+        myyntihinta double,
+        taiteilijaFK int UNSIGNED,
+        museoFK int UNSIGNED,
+        teoksenNimi varchar(200),
+        PRIMARY KEY (taideteos_id),
+        FOREIGN KEY (taiteilijaFK) REFERENCES taiteilijat(taiteilija_id),
+        FOREIGN KEY (museoFK) REFERENCES museot(museo_id)
+        )
+      `);
+      await conn.execute(`
+      INSERT INTO taideteokset (taideteos_id, tekovuosi, aihe, myyntihinta, taiteilijaFK, museoFK, teoksenNimi)
+        VALUES 
+        ("1", "1893", "Henkilökuva", "12000000", "1", "1", "Otahi"),
+        ("2", "1892", "Henkilökuva", "999999", "1","2", "When will you marry me"),
+        ("3", "1886", "eläin", "123123", "2","2","taistelevat metsot")
+      `);
+      console.log('...success!');
+    }
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -99,6 +99,66 @@ const artistPath = `${artistsPath}/:id`;
 const museumsPath = `${apiPath}/museums`;
 const museumPath = `${museumsPath}/:id`;
 
+// kaikki
+const allPath = `${apiPath}/all`;
+
+// GET /resource (KAIKKI)
+todos.get(allPath, checkAccept, async (ctx) => {
+  const url = Url.parse(ctx.url, true);
+  const { sort } = url.query;
+
+  const parseSortQuery = ({ urlSortQuery, whitelist }) => {
+    let query = '';
+    if (urlSortQuery) {
+      const sortParams = urlSortQuery.split(',');
+
+      query = 'ORDER BY ';
+      sortParams.forEach((param, index) => {
+        let trimmedParam = param;
+        let desc = false;
+
+        if (param[0] === '-') {
+          // Remove the first character
+          trimmedParam = param.slice(1);
+          // Set descending to true
+          desc = true;
+        }
+
+        // If parameter is not whitelisted, ignore it
+        // This also prevents SQL injection even without statement preparation
+        if (!whitelist.includes(trimmedParam)) return;
+
+        // If this is not the first sort parameter, append ', '
+        if (index > 0) query = query.concat(', ');
+
+        // Append the name of the field
+        query = query.concat(trimmedParam);
+
+        if (desc) query = query.concat(' DESC');
+      });
+    }
+    return query;
+  };
+  const orderBy = parseSortQuery({ urlSortQuery: sort, whitelist: ['id', 'text', 'done'] });
+
+  try {
+    const conn = await mysql.createConnection(connectionSettings);
+    const [data] = await conn.execute(`
+        SELECT *
+        FROM taideteokset
+        INNER JOIN taiteilijat ON taideteokset.taiteilijaFK = taiteilijat.taiteilija_id
+        INNER JOIN museot ON taideteokset.museoFK = museot.museo_id
+        ${orderBy}
+      `);
+
+    // Return all todos
+    ctx.body = data;
+  } catch (error) {
+    console.error('Error occurred:', error);
+    ctx.throw(500, error);
+  }
+});
+
 // GET /resource (TODOS)
 todos.get(todosPath, checkAccept, async (ctx) => {
   const url = Url.parse(ctx.url, true);
